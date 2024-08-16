5b2e9692a6996f5ea825640553162862e7f9c3ed
Ethan Bryson <ebryson09@gmail.com>
2024-08-16 02:02:31 -0700

    GUI state control

---
+++ src/core/menu.cpp
@@ -0,0 +1,359 @@
+#include "menu.h"
+
+#include "button.h"
+#include "input_interpreter.h"
+#include "render_instance.h"
+#include "save.h"
+
+#include <iostream>
+
+static void cycleIndex(std::vector<Menu::Option> options, int* index, int quantity) {
+	int beg = *index;
+
+	do {
+		(*index) += quantity;
+
+		// Maximum quantity movement first then try checking closer elements
+		if(quantity > 0)
+			quantity = 1;
+
+		else if(quantity < 0)
+			quantity = -1;
+
+		// Safe check index
+		if(*index < 0)
+			(*index) = options.size() - 1;
+		
+		else if(*index >= options.size()) 
+			(*index) = 0;		
+
+		// Cyclical check, if we loop back on starting point
+		if(*index == beg)
+			return;
+
+	}while(options[*index].name == "");
+}
+
+int Menu::Table(std::vector<Option> options, int columns, bool selectByRow, int* hover, int user, Rectangle area) {
+	int status = Wait;
+
+	Button::Config b = g::save.getButtonConfig(user);
+
+	// Move cursor
+	if(g::input.keyPressed[b.Up]) 
+		cycleIndex(options, hover, -columns);
+
+	if(g::input.keyPressed[b.Down]) 
+		cycleIndex(options, hover, columns);
+
+	if(selectByRow) {
+
+		// Ensure only first column is selected
+		if(*hover % columns != 0) 
+			*hover -= *hover % columns;
+
+	}else {
+
+		if(g::input.keyPressed[b.Left] && !selectByRow) 
+			cycleIndex(options, hover, -1);
+
+		if(g::input.keyPressed[b.Right] && !selectByRow) 
+			cycleIndex(options, hover, 1);		
+	}
+
+	// Safe check index, can be bad on the first table call
+	if(*hover < 0)
+		(*hover) = options.size() - 1;
+	
+	else if(*hover >= options.size()) 
+		(*hover) = 0;	
+
+	if(g::input.keyPressed[b.B]){
+		status = Accept;
+
+	}else if(g::input.keyPressed[b.D]) {
+		status = Decline;
+	}
+
+	// Draw Properties
+	Vector2 pos = Vector2(area.x, area.y);
+
+	int 	rows 				= options.size() / columns;
+	int 	selectedRow 		= (*hover) / columns;
+	float 	distanceScrolled 	= fontHeight * (selectedRow + 1);
+	float	bottomRow			= rows * fontHeight;
+
+    // Set view to scrolled distance
+	float scroll = 0;
+
+	// If last row extends past the maximum area then activate scroll
+	if(bottomRow > area.h) {
+
+	    if(distanceScrolled > bottomRow - area.h / 2) {
+	        scroll = bottomRow - area.h;
+
+	    }else if(distanceScrolled > area.h / 2) {
+	        scroll = distanceScrolled - area.h / 2;
+	    }		
+	}
+
+	sf::View view({
+		area.x, 
+		area.y + scroll, 
+		area.w, 
+		area.h
+	});
+
+	// View port is relative of screen coordinates
+	view.setViewport({
+		area.x / g::video.camera.screen_w, 
+		area.y / g::video.camera.screen_h, 
+		area.w / g::video.camera.screen_w, 
+		area.h / g::video.camera.screen_h
+	});
+
+	g::video.window.setView(view);
+
+	// Draw on screen
+	for(int i = 0; i < options.size(); i += columns) {
+
+		for(int j = 0; j < columns && i + j < options.size(); j ++) {
+			sf::Color color = sf::Color::White;
+
+			if(selectByRow) {
+
+				if(*hover == i)
+					color = sf::Color::Yellow;
+
+			}else {
+
+				if(*hover == i + j)
+					color = sf::Color::Yellow;
+			}
+
+	        sf::Text text;
+	        text.setFont(*g::save.getFont("Anton-Regular"));
+	        text.setCharacterSize(fontHeight);
+	        text.setString(options[i+j].name);
+	        text.setFillColor(color);
+
+	        // Ensure the text isn't overlapping into the next column
+	        float testSize = fontHeight;
+
+	        while(text.getLocalBounds().width > (area.w / columns)) {
+	        	text.setCharacterSize(testSize);
+	        	testSize --;
+	        }
+
+	        // Calculate free space within cell
+	        Vector2 freeSpace{
+	        	(area.w / columns) - text.getLocalBounds().width,
+	        	fontHeight - text.getLocalBounds().height
+	        };
+
+	        text.setPosition({pos.x + j * (area.w / columns) + freeSpace.x / 2, pos.y + freeSpace.y / 2});
+
+	        g::video.window.draw(text);
+		}
+        pos.y += fontHeight;
+	}
+
+	g::video.window.setView(g::video.window.getDefaultView());
+
+	return status;
+}
+
+int Menu::List(std::vector<Option> options, int* hover, int user, Rectangle area) {
+	return Table(options, 1, true, hover, user, area);
+}
+
+static int keyboardData[4];
+
+int Menu::Text(std::string* str, int user, Rectangle area) {
+
+	// Draw the string
+	sf::Text text;
+	text.setFont(*g::save.getFont("Anton-Regular"));
+	text.setCharacterSize(fontHeight);
+	text.setColor(sf::Color::White);	
+	text.setString(*str);
+
+    // Ensure the text isn't overlapping into the next column
+    float testSize = fontHeight;
+
+    while(text.getLocalBounds().width > area.w) {
+    	text.setCharacterSize(testSize);
+    	testSize --;
+    }
+
+    // Calculate free space within cell
+    Vector2 freeSpace{
+    	area.w - text.getLocalBounds().width,
+    	fontHeight - text.getLocalBounds().height
+    };
+
+	text.setPosition({area.x + freeSpace.x / 2, area.y + freeSpace.y});
+	g::video.window.draw(text);
+
+	// Adjust remaining area for the keyboard
+	area.y += fontHeight*2;
+	area.h -= fontHeight*2;
+
+	enum {
+		Delete 	= -1,
+		Enter	= -2,
+		Cancel	= -3
+	};
+
+	vector<Option> options;
+	options.push_back({"Q", 'Q'});
+	options.push_back({"W", 'W'});
+	options.push_back({"E", 'E'});
+	options.push_back({"R", 'R'});
+	options.push_back({"T", 'T'});
+	options.push_back({"Y", 'Y'});
+	options.push_back({"U", 'U'});
+	options.push_back({"I", 'I'});
+	options.push_back({"O", 'O'});
+	options.push_back({"P", 'P'});
+
+	options.push_back({"A", 'A'});
+	options.push_back({"S", 'S'});
+	options.push_back({"D", 'D'});
+	options.push_back({"F", 'F'});
+	options.push_back({"G", 'G'});
+	options.push_back({"H", 'H'});
+	options.push_back({"J", 'J'});
+	options.push_back({"K", 'K'});
+	options.push_back({"L", 'L'});
+	options.push_back({"Delete", Delete});
+
+	options.push_back({"Z", 'A'});
+	options.push_back({"X", 'S'});
+	options.push_back({"C", 'D'});
+	options.push_back({"V", 'F'});
+	options.push_back({"B", 'G'});
+	options.push_back({"N", 'H'});
+	options.push_back({"M", 'J'});
+	options.push_back({"K", 'K'});
+	options.push_back({"Space", ' '});
+	options.push_back({"Enter", Enter});
+
+	options.push_back({"Cancel", Cancel});
+
+	int res = Table(options, 10, false, &keyboardData[user], user, area);
+
+	if(res == Accept) {
+
+		if(options[keyboardData[user]].id == Enter) {
+			return Accept;
+
+		}else if(options[keyboardData[user]].id == Delete) {
+
+			if((*str).size() > 0) 
+				(*str).pop_back();			
+
+		}else if(options[keyboardData[user]].id == Cancel) {
+			return Decline;
+
+		}else {
+			(*str) += (char)options[keyboardData[user]].id;
+		}
+	}
+
+	return Wait;
+}
+
+int Menu::Motion(std::string* str, int side, int user, Rectangle area) {
+	Button::Config b = g::save.getButtonConfig(user);
+
+	// Draw the string
+	sf::Text text;
+	text.setFont(*g::save.getFont("Anton-Regular"));
+	text.setCharacterSize(fontHeight);
+	text.setColor(sf::Color::White);	
+	text.setString(*str);
+
+    // Ensure the text isn't overlapping into the next column
+    float testSize = fontHeight;
+
+    while(text.getLocalBounds().width > area.w) {
+    	text.setCharacterSize(testSize);
+    	testSize --;
+    }
+
+    // Calculate free space within cell
+    Vector2 freeSpace{
+    	area.w - text.getLocalBounds().width,
+    	fontHeight - text.getLocalBounds().height
+    };
+
+	text.setPosition({area.x + freeSpace.x / 2, area.y + freeSpace.y});
+	g::video.window.draw(text);
+
+	// Create a test player to get SOCD and motion inputs
+	Player test;
+	test.seatIndex = user;
+	test.state.button[0] = test.readInput();
+
+	string motion = "";
+
+	// Key pressed / released and someother key is also being concurrently held
+	if((g::input.keyPressed[b.Up] || g::input.keyPressed[b.Down] ||
+		g::input.keyPressed[b.Left] || g::input.keyPressed[b.Right] ||
+		g::input.keyReleased[b.Up] || g::input.keyReleased[b.Down] ||
+		g::input.keyReleased[b.Left] || g::input.keyReleased[b.Right]) &&
+
+		(g::input.keyHeld[b.Up] || g::input.keyHeld[b.Down] ||
+		g::input.keyHeld[b.Left] || g::input.keyHeld[b.Right])
+		) {
+
+	    Vector2 socd = test.getSOCD();
+	    socd.x *= side;
+	    motion += ('5' + (int)socd.x + (int)socd.y * 3);
+	}
+
+    if(g::input.keyPressed[b.A]) {
+
+    	if(g::input.keyHeld[b.B] || g::input.keyHeld[b.C] || g::input.keyHeld[b.D]) {
+        	motion += "+A"; 
+    	}else {
+        	motion += "A";    		
+    	}
+    }
+
+    if(g::input.keyPressed[b.B]) {
+
+    	if(g::input.keyHeld[b.A] || g::input.keyHeld[b.C] || g::input.keyHeld[b.D]) {
+        	motion += "+B"; 
+    	}else {
+        	motion += "B";    		
+    	}
+    }
+
+    if(g::input.keyPressed[b.C]) {
+
+    	if(g::input.keyHeld[b.A] || g::input.keyHeld[b.B] || g::input.keyHeld[b.D]) {
+        	motion += "+C"; 
+    	}else {
+        	motion += "C";
+    	}
+    }
+
+    if(g::input.keyPressed[b.D]) {
+
+    	if(g::input.keyHeld[b.A] || g::input.keyHeld[b.B] || g::input.keyHeld[b.C]) {
+        	motion += "+D";
+    	}else {
+        	motion += "D";
+    	}
+    }
+
+    if(motion.size() > 0)
+    	(*str) += motion;
+
+	if(g::input.keyPressed[b.Taunt])
+		return Accept;
+
+	return Wait;
+}
---
+++ src/core/menu.h
@@ -0,0 +1,30 @@
+#ifndef GAME_MENU_H
+#define GAME_MENU_H
+
+#include "math.h"
+
+#include <vector>
+#include <string>
+
+namespace Menu {
+
+	const float fontHeight = 30;
+
+	struct Option {
+		std::string name;
+		int id;
+	};
+
+	enum {
+		Accept,
+		Decline,
+		Wait
+	};
+
+	int Table(std::vector<Option> options, int columns, bool selectByRow, int* hover, int user, Rectangle area);
+	int List(std::vector<Option> options, int* hover, int user, Rectangle area);
+	int Text(std::string* str, int user, Rectangle area);
+	int Motion(std::string* str, int side, int user, Rectangle area);	
+};
+
+#endif
